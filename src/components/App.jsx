d8aea25fb81e3e35b6d73aae4aa1397e69b943df
mt-andrea <mateandicica@gmail.com>
2022-01-30 18:41:46 +0100

    notes update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from '../notes'
 
 function App(){
     return(<div>
         <Header />
-        <Note/>
+        {notes.map(n=>
+        <Note
+            key={n.key}
+            title={n.title}
+            content={n.content}/> )}
         <Footer/>
     </div>)
 }
